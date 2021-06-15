7b4efb76c511d27e8c2f340afb2577d7f2189c78
Matias Cuervo <matias.a.acuervo@gmail.com>
2021-06-15 08:43:14 -0300

    are u ready?

--- components/TodoItem.jsx
+++ components/TodoItem.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-import { StyleSheet, Text, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, TouchableOpacity,View} from 'react-native';
+import {MaterialIcons} from '@expo/vector-icons'
+
+
+
+
 const TodoItem = ({item,pressHandler}) => {
     return (
         <TouchableOpacity onPress={()=>pressHandler(item.key)}>
-            <Text style={styles.item}>{item.text}</Text>
+           <View style={styles.item}>
+           <MaterialIcons name='delete' size={20} color='#333'/>
+            <Text style={styles.itemtext}>{item.text}</Text>
+           </View> 
         </TouchableOpacity>
     )
 }
@@ -11,12 +19,15 @@ const TodoItem = ({item,pressHandler}) => {
 
 const styles= StyleSheet.create({
     item:{
+        flexDirection:'row',
         padding:16,
         marginTop:16,
         borderColor:'#bbb',
         borderWidth:1,
         borderStyle:'dashed',
         borderRadius:10
+    },itemText:{
+        marginLeft:10
     }
 })
 
